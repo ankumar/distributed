4f9075a3dbcdfe1bff95fd624e252b2c0618d1ff
Anil Kumar <an1kumar@gmail.com>
2021-12-09 12:24:45 -0800

    Update and rename dev/vscode/main.go to dev/temporal/main.go

---
+++ dev/temporal/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+    
+    "context"
+    "log"
+    
+    "github.com/google/uuid"
+    "go.temporal.io/sdk/client"
+)
+
+func main() {
+    fmt.Println("Hello, World!")
+}
